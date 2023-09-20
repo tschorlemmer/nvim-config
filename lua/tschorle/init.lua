c5b3b3753cc80a786acd14d371d24c060038ab48
Taylor Schorlemmer <taylor.schorlemmer@gmail.com>
2023-09-20 13:23:13 -0400

    add back windows features

--- lua/tschorle/init.lua
+++ lua/tschorle/init.lua
@@ -1,2 +1,3 @@
 require("tschorle.remap")
 require("tschorle.set")
+require("tschorle.powershell")
--- lua/tschorle/packer.lua
+++ lua/tschorle/packer.lua
@@ -62,6 +62,10 @@ return require('packer').startup(function(use)
           {'L3MON4D3/LuaSnip'},     -- Required
       }
   }
+
+  use("lervag/vimtex")
+
+
   if packer_bootstrap then
       require('packer').sync()
   end
