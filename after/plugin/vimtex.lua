006e86d0c739de6dc356d23d93110d94240cec2a
Taylor Schorlemmer <taylor.schorlemmer@gmail.com>
2023-10-14 23:03:31 -0400

    change behavior of vimtex toc

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -1,3 +1,18 @@
 vim.g.vimtex_complier_method = 'latexmk'
 vim.g.maplocalleader = ' '
 vim.g.vimtex_quickfix_autoclose_after_keystrokes = 2
+
+
+vim.g.vimtex_toc_config = {
+    layer_status = {
+        content = 1,
+        label = 0,
+        todo = 0,
+        include = 0,
+    },
+    hide_line_numbers = 0,
+    indent_levels = 1,
+    tocdepth = 1,
+    show_help = 0,
+
+}
